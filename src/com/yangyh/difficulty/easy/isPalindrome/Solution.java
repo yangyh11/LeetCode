0a226bc3d9b3020abd49f43b54bd3e94614ecb9b
yangyh <yangyh0609@cairenhui.com>
2019-06-14 17:11:08 +0800

    回文数

---
+++ src/com/yangyh/difficulty/easy/isPalindrome/Solution.java
@@ -0,0 +1,36 @@
+package com.yangyh.difficulty.easy.isPalindrome;
+
+/**
+ * @description: 判断一个整数是否是回文数
+ * @author: yangyh
+ * @create: 2019-06-14 17:07
+ * √ Accepted
+ *   √ 11509/11509 cases passed (56 ms)
+ *   √ Your runtime beats 52.26 % of java submissions
+ *   √ Your memory usage beats 77.16 % of java submissions
+ */
+public class Solution {
+
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        } else if (x < 10) {
+            return true;
+        } else if (x % 10 == 0) {
+            return false;
+        }
+
+        String str = String.valueOf(x);
+
+        char[] chars = str.toCharArray();
+        StringBuffer sb = new StringBuffer();
+
+        for (int i = chars.length-1; i >=0; i--) {
+            sb.append(chars[i]);
+        }
+        if (str.equals(sb.toString())) {
+            return true;
+        }
+        return false;
+    }
+}
