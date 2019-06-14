fe8f0eca734f386b39576ba4143dbc7705eb7e8c
yangyh <yangyh0609@cairenhui.com>
2019-06-14 17:11:29 +0800

    去掉多余的注释

--- src/com/yangyh/difficulty/easy/reverse/Solution3.java
+++ src/com/yangyh/difficulty/easy/reverse/Solution3.java
@@ -1,40 +1,19 @@
 package com.yangyh.difficulty.easy.reverse;
 
 /**
- * @lc app=leetcode.cn id=7 lang=java
- * <p>
  * [7] 整数反转
- * <p>
  * https://leetcode-cn.com/problems/reverse-integer/description/
- * <p>
- * algorithms
- * Easy (31.80%)
- * Total Accepted:    89.6K
- * Total Submissions: 281.7K
- * Testcase Example:  '123'
- * <p>
  * 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
- * <p>
  * 示例 1:
- * <p>
  * 输入: 123
  * 输出: 321
- * <p>
- * <p>
  * 示例 2:
- * <p>
  * 输入: -123
  * 输出: -321
- * <p>
- * <p>
  * 示例 3:
- * <p>
  * 输入: 120
  * 输出: 21
- * <p>
- * <p>
  * 注意:
- * <p>
  * 假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−2^31,  2^31 − 1]。请根据这个假设，如果反转后整数溢出那么就返回
  * 0。
  * @author: yangyh
--- src/com/yangyh/difficulty/easy/twosum/Solution2.java
+++ src/com/yangyh/difficulty/easy/twosum/Solution2.java
@@ -4,18 +4,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * @lc app=leetcode.cn id=1 lang=java
- *
  * [1] 两数之和
- *
  * https://leetcode-cn.com/problems/two-sum/description/
- *
- * algorithms
- * Easy (45.00%)
- * Total Accepted:    267.4K
- * Total Submissions: 594.2K
- * Testcase Example:  '[2,7,11,15]\n9'
- *
  * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
  *
  * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
@@ -26,8 +16,6 @@ import java.util.Map;
  *
  * 因为 nums[0] + nums[1] = 2 + 7 = 9
  * 所以返回 [0, 1]
- *
- *
  **/
 class Solution2 {
 
